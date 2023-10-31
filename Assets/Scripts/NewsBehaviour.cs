eef507f036eabd673b7d09a1da3e77c194e746d1
Yogui <zhidbcn@gmail.com>
2023-10-31 12:15:40 +0100

    Create NewsBehavior & Add NewsText gameobject

---
+++ Assets/Scripts/NewsBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsBehaviour : MonoBehaviour
+{
+    // News datas
+    private string _currentNews;
+    private List<Tuple<Sprite, string>> _newsAnswers; // sprite & explanation of each answer
+    private int _currentAnswer;
+
+    // private GameManager _gameManager;
+
+    public void UpdateNewsData()
+    {
+        // Send data to Letter
+        // Letter.UpdateLetter(_answersExplanations[_currentAnswer], _newsSprites[_currentAnswer], points);
+
+        // Get new data from GameManager
+        // var data = _gameManager.GetNextNews();
+    }
+
+}
