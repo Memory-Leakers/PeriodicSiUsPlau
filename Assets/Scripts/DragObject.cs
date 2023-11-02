0928603acf1a616b1b1c1703df17d9333b2e30cb
Yogui <zhidbcn@gmail.com>
2023-11-02 11:56:19 +0100

    Candle animation & picture smoother picked movement

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -8,8 +8,8 @@ public class DragObject : MonoBehaviour
 
     private bool _picked = false;
 
-    [SerializeField, Range(0.0f, 1.0f), Tooltip("The smaller, the smoother")]
-    private float _lerpInterval = 0.1f;
+    [SerializeField, Range(0.0f, 20.0f), Tooltip("The smaller, the slower")]
+    private float _lerpInterval = 15.0f;
 
     [SerializeField, Tooltip("X: height when object has been piked \nY: height when object has been drop")]
     private Vector2 _Ypos = new(0.5f, 0.15f);
@@ -42,11 +42,11 @@ public class DragObject : MonoBehaviour
     private void Update()
     {
         if (_picked)
-            transform.position = Vector3.Lerp(transform.position, GetMouseWorldPos() + _offset, _lerpInterval);
+            transform.position = Vector3.Lerp(transform.position, GetMouseWorldPos() + _offset, _lerpInterval * Time.deltaTime);
         else
         {
             transform.position = Vector3.Lerp(transform.position,
-                new(transform.position.x, _Ypos.y, transform.position.z), _lerpInterval * 0.5f);
+                new(transform.position.x, _Ypos.y, transform.position.z), _lerpInterval * Time.deltaTime);
         }
     }
 }
