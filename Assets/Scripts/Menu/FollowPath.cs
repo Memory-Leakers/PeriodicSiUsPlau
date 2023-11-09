fabeb939a4bbf2ca09b92952789717fab8c99386
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-11-09 17:33:50 +0100

    Update

--- Assets/Scripts/Menu/FollowPath.cs
+++ Assets/Scripts/Menu/FollowPath.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
 
+
     [SerializeField]Transform[] pathPoints;
     [SerializeField]float movementSpeed = 1.0f;
+    
+    Vector3 cameraPosition;
     int pathIndex = 0;
 
-    /*
-     * Sx = U*cos(angle)*t;
-       Sy = U*sin(angle)*t - 0.5*g*t^2;
-     */
     // Start is called before the first frame update
     void Start()
     {
         gameObject.transform.position = pathPoints[pathIndex].position;
+        cameraPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pathIndex >= pathPoints.Length) { pathIndex = pathPoints.Length - 1; }
 
-        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, pathPoints[pathIndex].position, movementSpeed * Time.deltaTime);
+        Vector3 currentPosition = gameObject.transform.position;
+
+        Vector3 nextPosition = pathPoints[pathIndex].position;
+
+        Vector3 interpolatedPosition = Vector3.Lerp(currentPosition, nextPosition, movementSpeed * Time.deltaTime);
+
+        gameObject.transform.position = interpolatedPosition;
 
         if (gameObject.transform.position == pathPoints[pathIndex].position)
         {
