cd272c0f1899be094095987a37687c039f60cd63
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-11-10 18:16:43 +0100

    StartMenu Finished

---
+++ Assets/Scripts/DragObjectCreditos.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class DragObjectCreditos : MonoBehaviour
+{
+    private Vector3 _offset;
+
+    private bool _picked = false;
+
+    private bool _inSelectArea = false;
+
+    private bool _interactible = true;
+
+    [SerializeField, Range(0.0f, 20.0f), Tooltip("The smaller, the slower")]
+    private float _lerpInterval = 15.0f;
+
+    [SerializeField, Tooltip("X: height when object has been piked \nY: height when object has been drop")]
+    private Vector2 _Ypos = new(0.5f, 0.15f);
+
+    // Newspaper slect area pivot
+    private Transform _pivot;
+
+    private Vector3 initialPos = Vector3.zero;
+
+    private void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!_interactible)
+            return;
+
+        // pick immediadly
+        transform.position = new(transform.position.x, _Ypos.x, transform.position.z);
+
+        _offset = transform.position - GetMouseWorldPos();
+
+        _picked = true;
+    }
+
+    private void OnMouseUp()
+    {
+        _picked = false;
+
+        if (_inSelectArea)
+        {
+            // selected
+        }
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void Update()
+    {
+        if (_picked)
+            transform.position = Vector3.Lerp(transform.position, GetMouseWorldPos() + _offset, _lerpInterval * Time.deltaTime);
+        else
+        {
+
+            transform.position = Vector3.Lerp(transform.position,
+               new(initialPos.x, _Ypos.y, initialPos.z), _lerpInterval * Time.deltaTime);
+
+        }
+    }
+
+}
